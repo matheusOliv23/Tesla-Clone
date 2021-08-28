4f45a89b9ab97b96ed9b2bb991d64a69de58ab79
matheusOliv23 <matheushenri1996@gmail.com>
2021-08-28 17:36:23 -0300

    scroll

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Getting Started with Create React App
+# Tesla Clone
 
 Instalação das dependências
 
--- src/components/Model/ModelSection/styles.ts
+++ src/components/Model/ModelSection/styles.ts
@@ -3,4 +3,6 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   height: 100vh;
+
+  scroll-snap-align: start;
 `
